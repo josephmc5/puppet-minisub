235de93cc8c4c3d7e23570ade9251d182cab95aa
josephmc5 <root@ubuntu.(none)>
2012-10-30 22:04:25 -0400

    quotes on external dns var, include nginx

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,10 +1,11 @@
 class minisub::web inherits minisub::params {
     if $proxy_nginx {
+	include nginx
         nginx::resource::location { 'minisub':
             ensure   => present,
             location_alias => "$base_dir/minisub/src/",
             location => '/minisub',
-            vhost    => $external_dns,
+            vhost    => "$external_dns",
         }
     }
 }
